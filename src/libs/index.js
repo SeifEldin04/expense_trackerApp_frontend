be6df76b02d6f405ee9ae6bae7481f4e11d33924
Seif Eldin <seifeldinn4@gmail.com>
2025-07-02 07:19:38 +0300

    Make fetchCountries loacl

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
+import countriesData from '../data/countries.json'
 
 export const maskAccountNumber = (accountNumber) => {
     if (typeof accountNumber !== "string" || accountNumber.length < 12) {
@@ -38,36 +39,60 @@ export const getDateSevenDaysAgo = () => {
     return sevenDaysAgo.toISOString().split("T")[0];
 };
 
+// export async function fetchCountries() {
+//     try {
+//         const response = await fetch("https://restcountries.com/v3.1/all");
+//         const data = await response.json();
+
+//         if (response.ok) {
+//             const countries = data.map((country) => {
+//                 const currencies = country.currencies || {};
+//                 const currencyCode = Object.keys(currencies)[0];
+
+//                 return {
+//                     country: country.name?.common || "",
+//                     flag: country.flags?.png || "",
+//                     currency: currencyCode || "",
+//                 };
+//             });
+
+//             const sortedCountries = countries.sort((a, b) =>
+//                 a.country.localeCompare(b.country)
+//             );
+
+//             return sortedCountries;
+//         } else {
+//             console.error(`Error: ${data.message}`);
+//             return [];
+//         }
+//     } catch (error) {
+//         console.error("An error occurred while fetching data:", error);
+//         return [];
+//     }
+// }
+
 export async function fetchCountries() {
-    try {
-        const response = await fetch("https://restcountries.com/v3.1/all");
-        const data = await response.json();
+  try {
+    const countries = countriesData.map((country) => {
+      const currencies = country.currencies || {};
+      const currencyCode = Object.keys(currencies)[0];
 
-        if (response.ok) {
-            const countries = data.map((country) => {
-                const currencies = country.currencies || {};
-                const currencyCode = Object.keys(currencies)[0];
+      return {
+        country: country.name?.common || "",
+        flag: country.flags?.png || "",
+        currency: currencyCode || "",
+      };
+    });
 
-                return {
-                    country: country.name?.common || "",
-                    flag: country.flags?.png || "",
-                    currency: currencyCode || "",
-                };
-            });
+    const sortedCountries = countries.sort((a, b) =>
+      a.country.localeCompare(b.country)
+    );
 
-            const sortedCountries = countries.sort((a, b) =>
-                a.country.localeCompare(b.country)
-            );
-
-            return sortedCountries;
-        } else {
-            console.error(`Error: ${data.message}`);
-            return [];
-        }
-    } catch (error) {
-        console.error("An error occurred while fetching data:", error);
-        return [];
-    }
+    return sortedCountries;
+  } catch (error) {
+    console.error("❌ Error while processing countries:", error);
+    return [];
+  }
 }
 
 export function generateAccountNumber() {
